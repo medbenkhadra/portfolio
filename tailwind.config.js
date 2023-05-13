4f54ac872f1f89261cacb16809586a54d80a5e59
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2023-05-13 14:45:12 +0100

    fixes and improve

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}","./**/*.{html,js}"],
-  theme: {
-    extend: {
-      fontFamily:{
-        "maven" :["Maven Pro","sans-serif"]
-      },
-      colors:{
-        textColor:"#5b5b5b",
-        grey:"#EEEEEE",
-        secondryColor:"#47465A",
-        headingcolor:"#353535"
-      }
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{html,js}",
+    ],
+    theme: {
+        extend: {
+            fontFamily:{
+              "maven" :["Maven Pro","sans-serif"]
+            },
+            colors:{
+              textColor:"#5b5b5b",
+              secondryColor:"#47465A",
+              headingcolor:"#1c1c1"
+            }
+          },
     },
-  },
-  plugins: [ require('@tailwindcss/forms')],
-}
+    plugins: [ require('@tailwindcss/forms')],
 
+  }
