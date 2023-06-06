c1718975e4217ce900835c5531540d21643ce128
mohamedbenkhadra <mohamedbenkhadra18@gmail.com>
2023-06-07 01:51:34 +0200

    update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -6,4 +6,30 @@ $(document).ready(function(){
         autoplayHoverPause:true
 
     });
+    $('.color-mode').click(function(){
+      $('body').toggleClass('dark-mode')
+    });
   });
+  
+  /*var options = {
+    Strings:["","",""],
+    typeSpeed:80,
+    fadeOut: true,
+    loop: true,
+    showCursor: false,
+  };
+  var typed = new Typed(".element",options);*/
+  const typed = new Typed('.element', {
+    strings: ['UI Specialist', 'Mohamed ben khadra','Web Designer'],
+    typeSpeed: 50,
+    fadeOut :true,
+    loop: true,
+    showCursor:false,
+    smartBackspace: true,
+  });
+  
+  var myElement = document.querySelector(".navbar");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init();
